d0c67055b38897173c4f06eef1218e39776e42e5
nurulalam <jack245200@gmail.com>
2022-10-27 02:09:09 +0600

    all file stucture ready

---
+++ src/Pages/LogIn/LogIn/LogIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LogIn = () => {
+  return <div>this is login</div>;
+};
+
+export default LogIn;
---
+++ src/Pages/Shared/CoursesCard/CoursesCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CoursesCard = () => {
+  return (
+    <div>
+      <h1>this Is CourseCard</h1>
+    </div>
+  );
+};
+
+export default CoursesCard;
